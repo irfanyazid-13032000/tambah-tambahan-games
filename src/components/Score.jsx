c784f3865b0a4f22a41c78785cd9eeae05788646
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-10-29 15:22:57 +0700

    ubah tampilan mobile

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,8 +2,8 @@ export default function Score({timeLeft,score,historyScore}){
   return(
     <div className="papannilai" style={{ display: timeLeft > 0 ? "none" : "block" }}>
          <p>Your Score :</p>
-         <h1>{score}</h1>
-         <h3>Highest score : {Math.max(...historyScore)}</h3>
+         <p>{score}</p>
+         <p>Highest score : {Math.max(...historyScore)}</p>
     </div>
   )
 }
